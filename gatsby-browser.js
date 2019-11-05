7d5db1801af80a45f359e6a9abaf525d619d2dba
Eder <edermunhozsantos@gmail.com>
2019-11-05 17:12:01 -0300

    feat: on service worker update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,3 +4,13 @@ const { registerLinkResolver } = require('gatsby-source-prismic-graphql')
 const { linkResolver } = require('./src/utils/linkResolver')
 
 registerLinkResolver(linkResolver)
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+  if (answer === true) {
+    window.location.reload()
+  }
+}
