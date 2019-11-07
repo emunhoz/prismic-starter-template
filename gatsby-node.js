6af18d6f444a3575c077b5be32c46df38fdf8264
Eder <edermunhozsantos@gmail.com>
2019-11-07 02:30:48 -0300

    use simplified promise wrapper

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,30 +1,41 @@
 const path = require(`path`)
 
+// graphql function doesn't throw an error so we have to check to check for the result.errors to throw manually
+const wrapper = promise =>
+  promise.then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+    return result
+  })
+
 exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions
+  const blogTemplate = path.resolve('src/templates/post.js')
 
   // Query our blog posts
-  const result = await graphql(`
-    {
-      prismic {
-        allPosts {
-          edges {
-            node {
-              img
-              author
-              _meta {
-                id
-                uid
+  const result = await wrapper(
+    graphql(`
+      {
+        prismic {
+          allPosts {
+            edges {
+              node {
+                img
+                author
+                _meta {
+                  id
+                  uid
+                }
               }
             }
           }
         }
       }
-    }
-  `)
+    `)
+  )
 
   const posts = result.data.prismic.allPosts.edges
-  const blogTemplate = path.resolve('src/templates/post.js')
 
   posts.forEach(edge => {
     createPage({
