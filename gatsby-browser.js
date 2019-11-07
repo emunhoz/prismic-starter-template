53a912bdcecf54146ced3329631d9feb4e09f51a
Eder <edermunhozsantos@gmail.com>
2019-11-07 02:49:30 -0300

    update configs

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,4 @@
 import 'lazysizes'
-require('prismjs/themes/prism-tomorrow.css')
 
 const { registerLinkResolver } = require('gatsby-source-prismic-graphql')
 const { linkResolver } = require('./src/utils/linkResolver')
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,61 +1,3 @@
-const pluginConfig = [
-  `gatsby-plugin-react-helmet`,
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      path: `${__dirname}/src/images`,
-      name: `images`
-    }
-  },
-  'gatsby-transformer-sharp',
-  'gatsby-plugin-sharp',
-  {
-    resolve: `gatsby-source-prismic-graphql`,
-    options: {
-      repositoryName: 'gatsby-prismic-escale'
-    }
-  },
-  {
-    resolve: `gatsby-transformer-prismic`,
-    options: {
-      plugins: [`gatsby-prismic-lazy-load`]
-    }
-  },
-  {
-    resolve: `gatsby-plugin-prefetch-google-fonts`,
-    options: {
-      fonts: [
-        {
-          family: `Open Sans`,
-          variants: [`300`, `400`, `600`, `700`]
-        }
-      ]
-    }
-  },
-  {
-    resolve: `gatsby-plugin-manifest`,
-    options: {
-      name: `Prismic Escale Template`,
-      short_name: `@escaledigital`,
-      start_url: `/`,
-      background_color: `#16202c`,
-      theme_color: `#16202c`,
-      display: `minimal-ui`,
-      icon: `src/images/gatsby-icon.png`
-    }
-  },
-  `gatsby-plugin-styled-components`,
-  {
-    resolve: 'gatsby-plugin-netlify-cache',
-    options: {
-      cachePublic: true
-    }
-  },
-  `gatsby-plugin-sitemap`,
-  'gatsby-plugin-offline',
-  'gatsby-plugin-netlify'
-]
-
 module.exports = {
   siteMetadata: {
     title: `Prismic Start Template`,
@@ -63,5 +5,48 @@ module.exports = {
     author: `Prismic Start Template`,
     siteUrl: `https://gatsby-prismic-example.netlify.com/`
   },
-  plugins: pluginConfig
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Prismic Escale Template`,
+        short_name: `@escaledigital`,
+        start_url: `/`,
+        background_color: `#16202c`,
+        theme_color: `#16202c`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Open Sans`,
+            variants: [`300`, `400`, `600`, `700`]
+          }
+        ]
+      }
+    },
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-prismic-graphql`,
+      options: {
+        repositoryName: 'gatsby-prismic-escale',
+        linkResolver: () => post => `/${post.uid}`
+      }
+    },
+    `gatsby-plugin-offline`
+  ]
 }
