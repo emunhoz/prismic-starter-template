5108780b1d0f62139007fce9270b1d2bdc22ef0a
Eder <edermunhozsantos@gmail.com>
2019-11-07 00:59:01 -0300

    Update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,66 @@
+const pluginConfig = [
+  `gatsby-plugin-twitter`,
+  `gatsby-plugin-react-helmet`,
+  `gatsby-transformer-json`,
+  `gatsby-plugin-sharp`,
+  `gatsby-transformer-sharp`,
+  `gatsby-remark-prismjs`,
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      path: `${__dirname}/src/images`,
+      name: `images`
+    }
+  },
+  {
+    resolve: `gatsby-source-prismic-graphql`,
+    options: {
+      repositoryName: 'gatsby-prismic-escale'
+    }
+  },
+  `gatsby-plugin-netlify`,
+  {
+    resolve: 'gatsby-plugin-netlify-cache',
+    options: {
+      cachePublic: true
+    }
+  },
+  {
+    resolve: `gatsby-transformer-prismic`,
+    options: {
+      plugins: [`gatsby-prismic-lazy-load`]
+    }
+  },
+  {
+    resolve: `gatsby-plugin-prefetch-google-fonts`,
+    options: {
+      fonts: [
+        {
+          family: `Open Sans`,
+          variants: [`300`, `400`, `600`, `700`]
+        }
+      ]
+    }
+  },
+  `gatsby-plugin-styled-components`,
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `Prismic Escale Template`,
+      short_name: `@escaledigital`,
+      start_url: `/`,
+      background_color: `#16202c`,
+      theme_color: `#16202c`,
+      display: `minimal-ui`,
+      icon: `src/images/gatsby-icon.png`
+    }
+  },
+  // this (optional) plugin enables Progressive Web App + Offline functionality
+  // To learn more, visit: https://gatsby.dev/offline
+  `gatsby-plugin-offline`,
+  `gatsby-plugin-sitemap`
+]
+
 module.exports = {
   siteMetadata: {
     title: `Prismic Start Template`,
@@ -5,66 +68,5 @@ module.exports = {
     author: `Prismic Start Template`,
     siteUrl: `https://gatsby-prismic-example.netlify.com/`
   },
-  plugins: [
-    `gatsby-plugin-twitter`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-json`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-    `gatsby-remark-prismjs`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/images`,
-        name: `images`
-      }
-    },
-    {
-      resolve: `gatsby-source-prismic-graphql`,
-      options: {
-        repositoryName: 'gatsby-prismic-escale'
-      }
-    },
-    `gatsby-plugin-netlify`,
-    {
-      resolve: 'gatsby-plugin-netlify-cache',
-      options: {
-        cachePublic: true
-      }
-    },
-    {
-      resolve: `gatsby-transformer-prismic`,
-      options: {
-        plugins: [`gatsby-prismic-lazy-load`]
-      }
-    },
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Open Sans`,
-            variants: [`300`, `400`, `600`, `700`]
-          }
-        ]
-      }
-    },
-    `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Prismic Escale Template`,
-        short_name: `@escaledigital`,
-        start_url: `/`,
-        background_color: `#16202c`,
-        theme_color: `#16202c`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`
-      }
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-sitemap`
-  ]
+  plugins: pluginConfig
 }
