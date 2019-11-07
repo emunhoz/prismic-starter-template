c8299e9cb9b6ee34fa71a192302df5fe2ffae5ac
Eder <edermunhozsantos@gmail.com>
2019-11-07 13:06:03 -0300

    Update dependecies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,89 @@
+require('dotenv').config()
+
+const pluginConfig = [
+  `gatsby-plugin-react-helmet`,
+  {
+    resolve: `gatsby-source-prismic-graphql`,
+    options: {
+      repositoryName: 'gatsby-prismic-escale',
+      linkResolver: () => post => `/${post.uid}`
+    }
+  },
+  {
+    // keep as first gatsby-source-filesystem plugin for gatsby image support
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      path: `${__dirname}/src/images`,
+      name: 'images'
+    }
+  },
+  `gatsby-transformer-sharp`,
+  `gatsby-plugin-sharp`,
+  {
+    resolve: 'gatsby-transformer-remark',
+    options: {
+      plugins: [
+        {
+          resolve: 'gatsby-remark-images',
+          options: {
+            // It's important to specify the maxWidth (in pixels) of
+            // the content container as this plugin uses this as the
+            // base for generating different widths of each image.
+            maxWidth: 960,
+            linkImagesToOriginal: false
+          }
+        },
+        `gatsby-remark-prismjs`
+      ]
+    }
+  },
+  {
+    resolve: `gatsby-plugin-prefetch-google-fonts`,
+    options: {
+      fonts: [
+        {
+          family: `Open Sans`,
+          variants: [`300`, `400`, `600`, `700`]
+        }
+      ]
+    }
+  },
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `Prismic Boilerplate Gatsby`,
+      short_name: `PBG`,
+      start_url: `/`,
+      background_color: `#1C2938`,
+      theme_color: `#1C2938`,
+      display: `minimal-ui`,
+      icon: `src/images/gatsby-icon.png`
+    }
+  },
+  `gatsby-plugin-styled-components`,
+  `gatsby-plugin-netlify`,
+  {
+    resolve: 'gatsby-plugin-netlify-cache',
+    options: {
+      cachePublic: true
+    }
+  },
+  `gatsby-plugin-offline`,
+  `gatsby-plugin-sitemap`
+]
+
+if (process.env.CONTEXT === 'production') {
+  const analytics = {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      head: false
+    }
+  }
+
+  pluginConfig.push(analytics)
+}
+
 module.exports = {
   siteMetadata: {
     title: `Prismic Start Template`,
@@ -5,48 +91,5 @@ module.exports = {
     author: `Prismic Start Template`,
     siteUrl: `https://gatsby-prismic-example.netlify.com/`
   },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`
-      }
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Prismic Escale Template`,
-        short_name: `@escaledigital`,
-        start_url: `/`,
-        background_color: `#16202c`,
-        theme_color: `#16202c`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`
-      }
-    },
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Open Sans`,
-            variants: [`300`, `400`, `600`, `700`]
-          }
-        ]
-      }
-    },
-    `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-source-prismic-graphql`,
-      options: {
-        repositoryName: 'gatsby-prismic-escale',
-        linkResolver: () => post => `/${post.uid}`
-      }
-    },
-    `gatsby-plugin-offline`
-  ]
+  plugins: pluginConfig
 }
